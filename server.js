67cc774e0e3cc764d80e2bf059cafe72d6423b29
mmacatul <mmacatul@users.noreply.github.com>
2016-11-21 14:08:41 -0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/GetOrdersCount', function (req, res) {
+    var uri = 'https://08bdb249b92b67b005959abaecc386d4:42f34ed5b53d0b0bd0292fdb9a463233@questnutrition.myshopify.com/admin/orders/count.json?financial_status=authorized';
+    var request = require("request");
+
+    request(uri, function (error, response, body) {
+        res.send(body);
+    }); 
+});
+
+var ip =  process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+http.listen(port, ip, function () {
+    console.log('listening on *: ' + port);
+});
