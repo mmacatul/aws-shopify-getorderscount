f28364b64d663a198eb2b307d8126186ae0b1b0e
mmacatul <mmacatul@users.noreply.github.com>
2016-11-21 19:12:52 -0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+http.createServer(function (request, response) {
+    request.resume();
+    request.on('end', function() {
+
+        response.writeHead(200, {
+            'Content-Type' : 'text/plain'
+        });
+        response.end('Hello HTTP!');
+    });
+}).listen(8080);
